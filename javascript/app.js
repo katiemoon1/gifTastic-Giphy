c0ae9249f2dc2c15ef89aafb1dd860ba1b4b38fa
Moon Child <kaitline.moon@gmail.com>
2018-09-06 18:33:59 -0400

    Created the on click event and called AJAX.

--- javascript/app.js
+++ javascript/app.js
@@ -4,15 +4,27 @@ $(document).ready(() => {
 $("button").on("click", function() {
     var item = $(this).attr("data-item");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=JxsL9Labq5Ixvtcc2cdoaKZC5Y9i3Dxf&limit=10"
-})
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=JxsL9Labq5Ixvtcc2cdoaKZC5Y9i3Dxf&limit=10";
 
-
-
-
-
-
-
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+        .then(function(response) {
+            var results = response.data;
+            
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='thing'>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var itemImage = $("<img>");
+                itemImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.prepend(p);
+                gifDiv.prepend(itemImage);
+                $("#gifs-show-here").prepend(gifDiv);
+            }
+    });
+});
 
 
 
