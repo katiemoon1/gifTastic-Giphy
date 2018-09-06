205d1330092ade3c4d51f400e6af563267728152
Moon Child <kaitline.moon@gmail.com>
2018-09-06 17:17:04 -0400

    Created buttons and linked API.

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,11 @@
 $(document).ready(() => {
 
+// Creating a click event for the category buttons
+$("button").on("click", function() {
+    var item = $(this).attr("data-item");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=JxsL9Labq5Ixvtcc2cdoaKZC5Y9i3Dxf&limit=10"
+})
 
 
 
